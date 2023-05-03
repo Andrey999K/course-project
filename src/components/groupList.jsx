a6c27c59f09694c6798964f51b44bf018d9534f9
Andrey999K <andrey1234xxxx@mail.ru>
2023-05-03 16:28:33 +0300

    refactor: component groupList

--- src/components/groupList.jsx
+++ src/components/groupList.jsx
@@ -2,16 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const GroupList = ({ items, valueProperty, contentProperty, onItemSelect, selectedItem }) => {
+  items = Array.isArray(items) ? items : Object.values(items);
   return (
     <ul className="list-group">
-      {Object.keys(items).map(item =>
-        <li
-          key={items[item][valueProperty]}
-          className={"list-group-item" + (items[item] === selectedItem ? " active" : "")}
-          onClick={() => onItemSelect(items[item])}
-          role="button"
-        >{items[item][contentProperty]}</li>
-      )}
+      {
+        items.map(item =>
+          <li
+            key={item[valueProperty]}
+            className={"list-group-item" + (selectedItem && item[valueProperty] === selectedItem[valueProperty] ? " active" : "")}
+            onClick={() => onItemSelect(item)}
+            role="button"
+          >{item[contentProperty]}</li>
+        )
+      }
     </ul>
   );
 };
